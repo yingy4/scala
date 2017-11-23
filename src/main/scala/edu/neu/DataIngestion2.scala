3ae8e4bee389784577a781af6434058903f46bd0
Mohamad <bagheri@bu.edu>
2017-11-22 22:17:38 -0500

    First Commit

---
+++ src/main/scala/edu/neu/DataIngestion2.scala
@@ -0,0 +1,10 @@
+package edu.neu
+
+object DataIngestion {
+
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+
+}
---
+++ src/test/scala/edu/neu/DataIngestion.scala
@@ -0,0 +1,8 @@
+package edu.neu
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class DataIngestion extends FlatSpec with Matchers{
+  (2+2) should equal (4)
+
+}
